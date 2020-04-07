f0cdefa171b3d759bd7c9ccd19a9ac5bbe28a14a
Lucas Bovolini <lbovolini94@gmail.com>
2020-04-07 18:46:09 -0300

    Initial commit

---
+++ src/main/java/com/github/lbovolini/servlet/App.java
@@ -0,0 +1,17 @@
+package com.github.lbovolini.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class App extends HttpServlet
+{
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        request.getRequestDispatcher("/pages/home.jsp").forward(request, response);
+    }
+}
